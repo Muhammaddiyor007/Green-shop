31b3e761d869eec576f95a12776ce283924d355c
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2025-04-22 11:48:40 +0500

    "Green-shop"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainLayout from './Layout/MainLayout'
-import Home from './pages/Home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Auth from './Components/Auth';
+
+import MainLayout from './Layout/MainLayout';
+import Address from './pages/UserProfile/myProducts/adress';
+import Home from './pages/Home/Home';
+import MyProducts from './pages/UserProfile/myProducts';
+import TrackOrder from './pages/UserProfile/trackorder';
+import WishList from './pages/UserProfile/wishlist';
+import Profil from './pages/UserProfile/Profil';
+
+import UserProfile from './pages/UserProfile';
 
 function App() {
 	return (
@@ -9,11 +18,19 @@ function App() {
 				<Routes>
 					<Route element={<MainLayout />}>
 						<Route path='/' element={<Home />} />
+						<Route element={<UserProfile />}>
+							<Route path='/profil' element={<Profil />} />
+							<Route path='/profil/myProducts' element={<MyProducts />} />
+							<Route path='/profil/tracorder' element={<TrackOrder />} />
+							<Route path='/profil/wishlist' element={<WishList />} />
+							<Route path='/profil/address' element={<Address />} />
+						</Route>
 					</Route>
+					<Route path='/auth' element={<Auth />} />
 				</Routes>
 			</BrowserRouter>
 		</>
-	)
+	);
 }
 
-export default App
+export default App;
