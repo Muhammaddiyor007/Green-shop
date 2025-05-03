b58476b4c5034d5b471c0375291b301a26b8c708
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2025-05-03 18:55:16 +0500

    "Green-shop"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Auth from './Components/Auth';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Auth from './Components/Auth'
 
-import MainLayout from './Layout/MainLayout';
-import Address from './pages/UserProfile/myProducts/adress';
-import Home from './pages/Home/Home';
-import MyProducts from './pages/UserProfile/myProducts';
-import TrackOrder from './pages/UserProfile/trackorder';
-import WishList from './pages/UserProfile/wishlist';
-import Profil from './pages/UserProfile/Profil';
+import MainLayout from './Layout/MainLayout'
+import Address from './pages/UserProfile/myProducts/adress'
+import Home from './pages/Home/Home'
+import MyProducts from './pages/UserProfile/myProducts'
+import TrackOrder from './pages/UserProfile/trackorder'
+import WishList from './pages/UserProfile/wishlist'
+import Profil from './pages/UserProfile/Profil'
 
-import UserProfile from './pages/UserProfile';
+import UserPofil from './pages/UserProfile'
+import Shoping from './pages/Shoping'
 
 function App() {
 	return (
@@ -18,19 +19,20 @@ function App() {
 				<Routes>
 					<Route element={<MainLayout />}>
 						<Route path='/' element={<Home />} />
-						<Route element={<UserProfile />}>
-							<Route path='/profil' element={<Profil />} />
+						<Route element={<UserPofil />}>
+							<Route path='/profil'element={<Profil/>} />
 							<Route path='/profil/myProducts' element={<MyProducts />} />
 							<Route path='/profil/tracorder' element={<TrackOrder />} />
 							<Route path='/profil/wishlist' element={<WishList />} />
 							<Route path='/profil/address' element={<Address />} />
 						</Route>
+						<Route path='/product-card' element={<Shoping/>}  />
 					</Route>
 					<Route path='/auth' element={<Auth />} />
 				</Routes>
 			</BrowserRouter>
 		</>
-	);
+	)
 }
 
-export default App;
+export default App
